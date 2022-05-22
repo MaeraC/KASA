f62ca59207bdd793d9a965702ed2ad8ea43276bc
Maera <baeitsmae@gmail.com>
2022-05-22 16:35:32 +0200

    Création des Routes

--- kasa-frontend/src/index.js
+++ kasa-frontend/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router , Route, Switch } from 'react-router-dom'
+import 'typeface-montserrat'
+import './styles/index.css'
+import Home from './pages/Home'
+import Header from './components/Header'
+import About from './pages/About'
+import Error from './pages/Error'
+import Location from './pages/Location'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/location/:idCard'>
+          <Location />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
