f2c722d55ef5a2dc0ff0496b087ee3ca3f6cc016
Maera <baeitsmae@gmail.com>
2022-05-26 13:17:34 +0200

    Page Location

--- kasa-frontend/src/pages/Location.jsx
+++ kasa-frontend/src/pages/Location.jsx
@@ -1,42 +1,48 @@
 import { locations } from '../datas/locations'
 import { useParams } from 'react-router-dom'
 import '../styles/Location.css'
+import Tags from '../components/Tags'
+import Rating from '../components/Rating'
+import Description from '../components/Description'
+import Equipements from '../components/Equipements'
+import Carroussel from '../components/Carroussel'
 
 function Location() {
     // Récupère l'id correspondant à l'url
     const { idCard } = useParams()
     // Récupère les données de la Card correspondant à l'id
     const myCard = locations.find((data) => data.id === idCard)
-    // Récupère chaque propriété correspondant à la Card
-    const { pictures, title, location, tags, host, rating, description, equipments } = myCard 
+    // Récupère les propriétés title, location, host correspondant à la Card
+    const { title, location, host } = myCard 
 
+    // On implémente les propriétés dans la page
     return (
         <main>
-            <header>
-
-            </header>
+            <section className="Carroussel">
+                <Carroussel />
+            </section>
+            
             <section className="presentation">
                 <div className="presentation-top">
                     <h1>{title}</h1>
                     <div className="profile">
-                        <p>{host.name}</p>
+                        <p className="name">{host.name}</p>
                         <img src={host.picture} alt={host.name} />
                     </div>
                 </div>
                 <p className="localisation">{location}</p>
                 <div className="presentation-bottom">
-                    <div className="tags">
-                        ???
-                    </div>
-                    <div className="rating">{rating}</div>
+                    <Tags />
+                    <Rating />
                 </div>
-                
+            </section>
+
+            <section className="description-blocs">
+                <Description />
+                <Equipements />
             </section>
         </main>
-        
     )
-
-           
 }
 
 export default Location
